3a488f6c0f3ce7a62e5c0d59f96602447c6a450c
Dominic Covelli <114621376+dom-covelli@users.noreply.github.com>
2023-03-14 17:46:41 -0700

    Update 3 - Adds all yearly wind cfs together per location with leap years.py

--- rex/Data processing with leap years/3 - Adds all yearly wind cfs together per location with leap years.py
+++ rex/Data processing with leap years/3 - Adds all yearly wind cfs together per location with leap years.py
@@ -24,19 +24,19 @@ for folder in docs.iterdir():
         print(year[3])
 
         if year[3] == ' 2000':
-            data = pd.read_csv(file, sep=",", skiprows=17550, nrows=8783)
+            data = pd.read_csv(file, sep=",", skiprows=17573, nrows=8785)
         elif year[3] == ' 2004':
-            data = pd.read_csv(file, sep=",", skiprows=17550, nrows=8783)
+            data = pd.read_csv(file, sep=",", skiprows=17573, nrows=8785)
         elif year[3] == ' 2008':
-            data = pd.read_csv(file, sep=",", skiprows=17550, nrows=8783)
+            data = pd.read_csv(file, sep=",", skiprows=17573, nrows=8785)
         elif year[3] == ' 2012':
-            data = pd.read_csv(file, sep=",", skiprows=17550, nrows=8783)
+            data = pd.read_csv(file, sep=",", skiprows=17573, nrows=8785)
         elif year[3] == ' 2016':
-            data = pd.read_csv(file, sep=",", skiprows=17550, nrows=8783)
+            data = pd.read_csv(file, sep=",", skiprows=17573, nrows=8785)
         elif year[3] == ' 2020':
-            data = pd.read_csv(file, sep=",", skiprows=17550, nrows=8783)
+            data = pd.read_csv(file, sep=",", skiprows=17573, nrows=8785)
         else:
-            data = pd.read_csv(file, sep=",", skiprows=17526, nrows=8759)
+            data = pd.read_csv(file, sep=",", skiprows=17525, nrows=8761)
 
         print(data)
         #Create a new csv named after the folder in the path docs
