ec18fb836293b9bcf33a036ca00f8aefc6120d19
Dominic Covelli <114621376+dom-covelli@users.noreply.github.com>
2023-04-13 17:24:22 -0700

    Update Contour plot land use.py

--- Code for graphs/Contour plot land use.py
+++ Code for graphs/Contour plot land use.py
@@ -9,6 +9,7 @@ from matplotlib.colors import ListedColormap
 import pickle
 import os
 import pandas as pd
+from matplotlib import ticker
 
 
 path = '/Users/Dominic/output_data/1_year_e1_contour/'
@@ -52,36 +53,56 @@ for file in os.listdir():
             fuelcell = cell['fuel_cell']
             #print(fuelcell)
         
+            print(file_path)
+            #all the above values are in GW
+            #Account for the fact that 3.9653305GW solar can be built on existing developments, thus taking up no more land
+            solar = solar - 3.9653305
+            if solar < 0:
+                solar = 0
+            
+            #Account for the fact that (1/6) GW of onwind can be built per GW of solar, since turbines can be built on the same land as PV
+            if solar > 0:
+                wind = wind - (solar/6)
+            else:
+                wind = wind
+
             sys_land = (wind*121.4058)+(solar*20.2343)+(battery*0.092916)+(h2store*0.0046417)+(fuelcell*0.250905)
             print(sys_land)
-        
+
             #Append the value of sys_land to sys_land_use
             sys_land_use = np.append(sys_land_use, sys_land)
 
 print(sys_land_use)
 
+#Below variable appends the land use cases to an array with one exorbinant value, which represents the 0batt 0h2 case that is infeasible and thus has no pickle file
+sys_land_use2 = np.array([100000])
+sys_land_use2 = np.append(sys_land_use2, sys_land_use)
 
-x = [0, 6.66,13.32,20]
-y = [6.66,13.32,20]
+print(sys_land_use2)
+
+
+x = [0, 6.66,13.32,20,50,200]
+y = [0, 3.33,6.66,13.32,20,26.66,33.32]
 X, Y = np.meshgrid(y,x)
 
 print(X) #x is battery
 print(Y) #y is H2
 
 
-sys_land_use = sys_land_use.reshape(4,3)
-print(sys_land_use)
+sys_land_use2 = sys_land_use2.reshape(6,7)
+print(sys_land_use2)
 
 #Make Contour Plot
 
-plt.contourf(X, Y, sys_land_use, 4, cmap='OrRd')
+#plt.contourf(X, Y, sys_land_use2, 100, cmap='OrRd', locator=ticker.LogLocator())
+plt.contourf(X, Y, sys_land_use2, 100, cmap='OrRd')
 plt.colorbar(label='System Land Use (km^2)');
 
 #make labels
 plt.xlabel('Battery Storage (MWh)')
 plt.ylabel('H2 Storage (MWh)')
 plt.title('System Land Use Under Fixed Battery \n and H2 Storage Conditions')
-plt.xticks(np.arange(6.66, 21, step=4))
-plt.yticks(np.arange(0, 21, step=5))
-#plt.savefig('System Land Use Contour Plot.png', dpi=300)
+plt.xticks(np.arange(0, 36, step=6))
+plt.yticks(np.arange(0, 201, step=25))
+plt.savefig('System Land Use Contour Plot at origin linear scale.png', dpi=300)
 plt.show()
