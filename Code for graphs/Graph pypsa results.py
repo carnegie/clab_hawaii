87d32f7e7a26bbfbc0ac211e349331cbf42b52de
Dominic Covelli <114621376+dom-covelli@users.noreply.github.com>
2023-04-18 12:17:48 -0700

    Update Graph pypsa results.py

--- Code for graphs/Graph pypsa results.py
+++ Code for graphs/Graph pypsa results.py
@@ -28,18 +28,114 @@ solar_costs = []
 batt_costs = []
 pgp_costs = []
 
-cases = listdir('/Users/Dominic/Desktop/Oahu Results/(Pypsa) costs using mean nsrdb and wind, 14 year/plotting/')
-print(cases)
+no_pgp = '/Users/Dominic/Desktop/fourteen_year_system_costs/fourteen_year_yes_normal_no_pgp.pickle'
+pgp_above = '/Users/Dominic/Desktop/fourteen_year_system_costs/fourteen_year_above_storage_yes_normal_all_mw_e6.pickle'
+pgp_under = '/Users/Dominic/Desktop/fourteen_year_system_costs/fourteen_year_yes_normal_below_H2.pickle'
 
-sys_costs = [0.180665526, 0]
-wind_costs = [0.022059533, 0]
-solar_costs = [0.1150,0]
-batt_costs = [0.0435892369, 0]
-pgp_costs = [0,0]
+with open(no_pgp, 'rb') as f:
+      data = pickle.load(f)
+      component_results = data['component results']
+      #print(component_results)
+      spending = component_results['Capital Expenditure']
+      #print(spending)
+      gen = spending['Generator']
+      #print(gen)
+      wind = gen['onwind']
+      #print(wind)
+      solar = gen['solar']
+      #print(solar)
+      batt = spending['StorageUnit']
+      #print(batt)
+      battery = batt['battery']
 
+      wind_cost = wind / (122710*1000)
+      solar_cost = solar / (122710*1000)
+      battery_cost = battery / (122710*1000)
+
+      wind_costs.append(wind_cost)
+      solar_costs.append(solar_cost)
+      batt_costs.append(battery_cost)
+      pgp_costs.append(0.0)
+with open(pgp_above, 'rb') as f:
+      data1 = pickle.load(f)
+      component_results = data1['component results']
+      #print(component_results)
+      spending = component_results['Capital Expenditure']
+      #print(spending)
+      gen = spending['Generator']
+      #print(gen)
+      wind = gen['onwind']
+      #print(wind)
+      solar = gen['solar']
+      #print(solar)
+      batt = spending['StorageUnit']
+      #print(batt)
+      battery = batt['battery']
+      #print(battery)
+      h2 = spending['Store']
+      #print(h2)
+      h2store = h2['h2_storage']
+      #print(h2store)
+      cell = spending['Link']
+      #print(cell)
+      fuelcell = cell['fuel_cell']
+      #print(fuelcell)
+      electrolysis = cell['electrolysis']
+
+      wind_cost = wind / (122710*1000)
+      solar_cost = solar / (122710*1000)
+      battery_cost = battery / (122710*1000)
+      pgp_price = ((h2store/(122710*1000)) + (fuelcell/(122710*1000)) + (electrolysis/(122710*1000)))
+      
+      wind_costs.append(wind_cost)
+      solar_costs.append(solar_cost)
+      batt_costs.append(battery_cost)
+      pgp_costs.append(pgp_price)
+
+
+with open(pgp_under, 'rb') as f:
+      data2 = pickle.load(f)
+      component_results = data2['component results']
+      #print(component_results)
+      spending = component_results['Capital Expenditure']
+      #print(spending)
+      gen = spending['Generator']
+      #print(gen)
+      wind = gen['onwind']
+      #print(wind)
+      solar = gen['solar']
+      #print(solar)
+      batt = spending['StorageUnit']
+      #print(batt)
+      battery = batt['battery']
+      #print(battery)
+      h2 = spending['Store']
+      #print(h2)
+      h2store = h2['h2_storage']
+      #print(h2store)
+      cell = spending['Link']
+      #print(cell)
+      fuelcell = cell['fuel_cell']
+      #print(fuelcell)
+      electrolysis = cell['electrolysis']    
+
+      wind_cost = wind / (122710*1000)
+      solar_cost = solar / (122710*1000)
+      battery_cost = battery / (122710*1000)
+      pgp_price = ((h2store/(122710*1000)) + (fuelcell/(122710*1000)) + (electrolysis/(122710*1000)))
+      
+      wind_costs.append(wind_cost)
+      solar_costs.append(solar_cost)
+      batt_costs.append(battery_cost)
+      pgp_costs.append(pgp_price)
+      
+print(wind_costs)
+print(solar_costs)
+print(batt_costs)
+print(pgp_costs)
 
 #Prep for plotting    
-N = 2
+N = 3
 ind = np.arange(N)    # the x locations for the groups
 width = 0.7       # the width of the bars: can also be len(x) sequence
 
@@ -67,7 +163,7 @@ p3 = ax1.bar(ind, data3, width, bottom=data1+data2, color=batt_q)
 p4 = ax1.bar(ind, data4, width, bottom=data1+data2+data3, color=pgp_q)
 
 
-plt.ylabel('System cost ($/kWh)')
+plt.ylabel('System Cost ($/kWh)')
 plt.xticks(rotation=45, ha='right')
 
 ax = plt.gca() 
@@ -75,13 +171,16 @@ ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 #plt.grid(True, which = 'major', axis = 'y', c = 'lightgray')
 
-ax.set_ylim(0, 0.4)
-ax.yaxis.set_major_locator(ticker.MultipleLocator(0.05))
-ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.01))
+ax.set_ylim(0, 0.2)
+ax.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
+ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.04))
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-ax.set_title('PYPSA - Oahu Using Means from NSRDB and WIND', y=1.05)
+ax.set_title('Oahu Optimized Renewable System Cost', y=1.05)
 
-plt.xticks(ind, ('PV, Wind, Li-ion Battery', 'PV, Wind, Li-ion Battery, PGP'))
+#plt.xticks(ind, ('PV, Wind, Li-ion Battery', 'PV, Wind, Li-ion Battery, PGP (Above Ground Storage)', 'PV, Wind, Li-ion Battery, PGP (Below Ground Storage)'))
+plt.xticks(ind, ('No PGP', 'PGP (Aboveground Storage)', 'PGP (Underground Storage)'))
+y = ['Solar', 'Wind', 'Battery', 'PGP']
+ax1.legend(y, loc="upper right")
 
 #xlocs=[0,1]
 #for i, v in enumerate(sys_costs):
@@ -92,5 +191,5 @@ plt.xticks(ind, ('PV, Wind, Li-ion Battery', 'PV, Wind, Li-ion Battery, PGP'))
 ##======================================================================================================
 
 
-plt.savefig('Oahu Pypsa pv wind battery.jpg', dpi = 300, bbox_inches='tight')
+plt.savefig('Oahu Optimized Renewable System Cost.jpg', dpi = 300, bbox_inches='tight')
 plt.show()
