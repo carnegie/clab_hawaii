989372b34a8b9a85a8f31e00b1e774e8d9120f6e
Dominic Covelli <114621376+dom-covelli@users.noreply.github.com>
2023-04-18 17:11:51 -0700

    Update Graph land use.py

--- Code for graphs/Graph land use.py
+++ Code for graphs/Graph land use.py
@@ -4,7 +4,7 @@ This code will calculate and graph the land use of each technology in the Oahu s
 from __future__ import division
 import numpy as np
 from os import listdir
-
+import os
 import pickle
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
@@ -16,45 +16,134 @@ from matplotlib.dates import drange
 from matplotlib.ticker import FormatStrFormatter
 import pandas as pd
 
-
-##==========================================
-# Tech colors
-##==========================================
-
-solar_q = 'orange'
-wind_q = 'blue'
-batt_q = 'purple'
-h2storage_q = 'pink'
-fuel_cell_q = 'green'
+path1 = '/Users/Dominic/desktop/fourteen_year_land_use/fourteen_year_above_storage_no_normal_all_mw_e6.pickle'
+path2 = '/Users/Dominic/desktop/fourteen_year_land_use/fourteen_year_no_normal_space_optimization.pickle'
 
 
-wind_land = [(1613.00*(30*0.00404686)),(266.77*(30*0.00404686))]
-print(wind_land)
-solar_land  = [(6500.65*(5*0.00404686)),(4907.54*(5*0.00404686))]
-print(solar_land)
-batt_land  = [(6634.80*(0.02296*0.00404686)),(3805.59*(0.02296*0.00404686))]
-print(batt_land)
-fuel_cell_land =[(0*(0.062*0.00404686)),(687.63*(0.062*0.00404686))]
-print(fuel_cell_land)
+combo_wind_solar = np.array([])
+wind_arr = np.array([])
+battery_arr = np.array([])
+h2store_arr = np.array([])
+fuelcell_arr = np.array([])
+
+with open(path1, 'rb') as f:
+    data = pickle.load(f)
+    component_results = data['component results']
+    #print(component_results)
+    land_use = component_results['Optimal Capacity']
+    #print(land_use)
+    gen = land_use['Generator']
+    #print(gen)
+    wind = gen['onwind']
+    wind = wind*(10**3)
+    print(wind)
+    solar = gen['solar']
+    solar = solar*(10**3)
+    print(solar)
+    batt = land_use['StorageUnit']
+    #print(batt)
+    battery = batt['battery']
+    #print(battery)
+    h2 = land_use['Store']
+    #print(h2)
+    h2store = h2['h2_storage']
+    #print(h2store)
+    cell = land_use['Link']
+    #print(cell)
+    fuelcell = cell['fuel_cell']
+    #print(fuelcell)
+    
+    #all the above values are in GW
+    #Account for the fact that 3.9653305GW solar can be built on existing developments, thus taking up no more land
+    solar = solar - 3.9653305
+    if solar < 0:
+        solar = 0
+    
+    #Account for the fact that (1/6) GW of onwind can be built per GW of solar, since turbines can be built on the same land as PV
+    if solar > 0:
+        wind = wind - (solar/6)
+    else:
+        wind = wind
+
+    combo_wind_solar = np.append(combo_wind_solar, solar)
+    wind_arr = np.append(wind_arr, wind)
+    battery_arr = np.append(battery_arr, battery)
+    h2store_arr = np.append(h2store_arr, h2store)
+    fuelcell_arr = np.append(fuelcell_arr, fuelcell)
+'''  
+#======================================================== If you want to plot two side-by-side =====================================  
+with open(path2, 'rb') as f:
+    data = pickle.load(f)
+    component_results = data['component results']
+    #print(component_results)
+    land_use = component_results['Optimal Capacity']
+    #print(land_use)
+    gen = land_use['Generator']
+    #print(gen)
+    wind = gen['onwind']
+    wind = wind/(10**3)
+    print(wind)
+    solar = gen['solar']
+    solar = solar/(10**3)
+    print(solar)
+    batt = land_use['StorageUnit']
+    #print(batt)
+    battery = batt['battery']
+    #print(battery)
+    h2 = land_use['Store']
+    #print(h2)
+    h2store = h2['h2_storage']
+    #print(h2store)
+    cell = land_use['Link']
+    #print(cell)
+    fuelcell = cell['fuel_cell']
+    #print(fuelcell)
+    
+    #all the above values are in GW
+    #Account for the fact that 3.9653305GW solar can be built on existing developments, thus taking up no more land
+    solar = solar - 3.9653305
+    if solar < 0:
+        solar = 0
+    
+    #Account for the fact that (1/6) GW of onwind can be built per GW of solar, since turbines can be built on the same land as PV
+    if solar > 0:
+        wind = wind - (solar/6)
+    else:
+        wind = wind
+
+combo_wind_solar = np.append(combo_wind_solar, solar)
+wind_arr = np.append(wind_arr, wind)
+battery_arr = np.append(battery_arr, battery)
+h2store_arr = np.append(h2store_arr, h2store)
+fuelcell_arr = np.append(fuelcell_arr, fuelcell)
 
 
+    #Append the value of sys_land to sys_land_use
+    #sys_land_use = np.append(sys_land_use, sys_land)
+wind_arr = (wind_arr*121.4058)
+combo_wind_solar = (combo_wind_solar*20.2343)
+battery_arr = (battery_arr*0.092916/1000)
+h2store_arr = (h2store_arr*0.0046417/1000)
+fuelcell_arr = (fuelcell_arr*0.250905/1000)
+'''
 
-cases = listdir('/Users/Dominic/Desktop/Oahu Results/(Pypsa) costs using mean nsrdb and wind, 14 year/plotting/')
+wind_arr = (wind_arr*121.4058)
+combo_wind_solar = (combo_wind_solar*20.2343)
+battery_arr = (battery_arr*0.092916/1000)
+h2store_arr = (h2store_arr*0.0046417/1000)
+fuelcell_arr = (fuelcell_arr*0.250905/1000)
 
-
-#Prep for plotting    
 N = 2
-ind = np.arange(N)    # the x locations for the groups
-width = 0.7       # the width of the bars: can also be len(x) sequence
+#ind = np.arange(N)    
 
-data1 = np.array(wind_land)
-data2 = np.array(solar_land)
-data3 = np.array(batt_land) 
-data4 = np.array(fuel_cell_land)
+data1 = combo_wind_solar
+data2 = wind_arr
+data3 = battery_arr
+data4 = fuelcell_arr
+data5 = h2store_arr
 
-##======================================================================================================
-# Make bar graph
-##======================================================================================================
+ind = ['Least Cost System']
+width = 0.7
 
 params = {'legend.fontsize': 'medium',
          'axes.labelsize': 'large',
@@ -63,16 +152,19 @@ params = {'legend.fontsize': 'medium',
          'ytick.labelsize':'large'}
 pylab.rcParams.update(params)
 
-fig = plt.figure(figsize=(8,10))
+fig = plt.figure(figsize=(6,10))
 ax1 = plt.subplot2grid((2, 2), (0, 0), colspan=1, rowspan=1)
-p1 = ax1.bar(ind, data1, width, color=wind_q)
-p2 = ax1.bar(ind, data2, width, bottom=data1, color=solar_q)
-p3 = ax1.bar(ind, data3, width, bottom=data1+data2, color=batt_q)
-p4 = ax1.bar(ind, data4, width, bottom=data1+data2+data3, color=fuel_cell_q)
+p1 = ax1.bar(ind, data1, width, color='orange', hatch='///', edgecolor='blue')
+#https://stackoverflow.com/questions/53849888/make-patches-bigger-used-as-legend-inside-matplotlib
+p2 = ax1.bar(ind, data2, width, bottom=data1, color='blue',edgecolor='blue')
+p3 = ax1.bar(ind, data3, width, bottom=data1+data2, color='purple', edgecolor='purple')
+p4 = ax1.bar(ind, data4, width, bottom=data1+data2+data3, color='green')
+p5 = ax1.bar(ind, data5, width, bottom=data1+data2+data3+data4, color='pink')
 
 
 
-plt.ylabel('System Land Use (km^2)')
+plt.ylabel('System Land Use (km^2)',labelpad = 6)
+
 plt.xticks(rotation=45, ha='right')
 
 ax = plt.gca() 
@@ -80,18 +172,17 @@ ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 #plt.grid(True, which = 'major', axis = 'y', c = 'lightgray')
 
-ax.set_ylim(0, 350)
+ax.set_ylim(0, 200)
 ax.yaxis.set_major_locator(ticker.MultipleLocator(50))
 ax.yaxis.set_minor_locator(ticker.MultipleLocator(10))
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 ax.set_title('Oahu Renewable Energy System Land Use', y=1.05)
 
-plt.xticks(ind, ('Least Cost System', 'Least Cost Below Ground H2 Storage System'))
+#plt.xticks(ind, ('Least Cost System', 'Space Optimized System'))
 
-
-y = ['Wind','Solar', 'Battery', 'Fuel Cell']
-ax1.legend(y, loc="upper right")
-
+#y = ['Wind and Solar','Wind Only', 'Battery', 'Fuel Cell', 'Hydrogen Storage']
+y = ['Wind and Solar','Wind Only', 'Battery']
+ax1.legend(y, loc="center right", bbox_to_anchor=(1.9, 0.5), fontsize = 12)
 ##======================================================================================================
 # Save plots as jpg
 ##======================================================================================================
